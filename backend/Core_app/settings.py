b426c4d805b168375b7ecd4b781a78a1eddcce46
Kenbaz <kbassey016@gmail.com>
2025-07-14 19:40:19 +0100

    updated db config in settings

--- backend/Core_app/settings.py
+++ backend/Core_app/settings.py
@@ -4,9 +4,12 @@ import secrets
 from pathlib import Path
 from urllib.parse import urlparse
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT', 'development')
 
 load_dotenv(BASE_DIR / '.env')
 
@@ -113,16 +116,33 @@ WSGI_APPLICATION = 'Core_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+# Database Configuration
+if ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DB_PRODUCTION_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+
+    # Ensure SSL is required for PostgreSQL in production
+    if DATABASES['default']:
+        DATABASES['default']['OPTIONS'] = {
+            'sslmode': 'require',
+        }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '5433'),
+        }
+    }
 
 
 # Password validation
